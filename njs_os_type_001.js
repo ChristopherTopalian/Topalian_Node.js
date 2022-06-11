f23b2363534d313702492517535747de8af6d035
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:19:29 -0400

    Update njs_os_type_001.js

--- njs_os_type_001.js
+++ njs_os_type_001.js
@@ -1,4 +1,5 @@
 // Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
 // node njs_os_type_001.js
 // shows the name of the operating system using console.log
 
