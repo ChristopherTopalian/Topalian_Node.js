7aa3c72670458bc0110ed6c79da596af93bef7ec
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:46:50 -0400

    Add files via upload

---
+++ njs_os_dirname_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_dirname_001.js
+// shows the directory name using console.log
+
+const os = require('os');
+
+console.log(__dirname);
