ddb5c6a6d5d9adf6d81753c72804b122d98b42d5
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:25:45 -0400

    Add files via upload

---
+++ njs_os_version_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_version_001.js
+// shows the verision of the operating system using console.log
+
+const os = require('os');
+
+console.log(os.version());
