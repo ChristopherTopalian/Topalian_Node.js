df163428e0066b51e4bd4f7faf417a20f7233ed5
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:26:44 -0400

    Delete njs_os_version_001.js

--- njs_os_version_001.js
+++
@@ -1,8 +0,0 @@
-// Dedicated to God the Father
-// All Rights Reserved Christopher Topalian Copyright 2000-2022
-// node njs_os_version_001.js
-// shows the version of the operating system using console.log
-
-const os = require('os');
-
-console.log(os.version());
