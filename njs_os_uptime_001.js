1f58f2eed32f359beb8b0b183a6fa774494f1be5
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:26:03 -0400

    Delete njs_os_uptime_001.js

--- njs_os_uptime_001.js
+++
@@ -1,7 +0,0 @@
-// Dedicated to God the Father
-// node njs_os_uptime_001.js
-// shows the uptime of the operating system in seconds using console.log
-
-const os = require('os');
-
-console.log(os.uptime());
