7ea1751f8f0663f8c36cb24ab4ce869fdfe6362d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:44:13 -0400

    Add files via upload

---
+++ njs_os_freemem_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_freemem_001.js
+// shows the amount of free system memory (ram) in bytes, as an integer, using console.log
+
+const os = require('os');
+
+console.log(os.freemem());
