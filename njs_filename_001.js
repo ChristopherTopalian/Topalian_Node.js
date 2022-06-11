7d1c206fd82f5df1c741c5d776802968fea06f62
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:07:37 -0400

    Update njs_filename_001.js

--- njs_filename_001.js
+++ njs_filename_001.js
@@ -1,6 +1,6 @@
 // Dedicated to God the Father
 // All Rights Reserved Christopher Topalian Copyright 2000-2022
 // node njs_filename_001.js
-// shows the full path file name using console.log
+// shows the absolute path file name using console.log
 
 console.log(__filename);
