ab5e891e178e6c7dd07be3c0f1a85e40a05d15b4
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:35:06 -0400

    Update njs_os_totalmem_001.js

--- njs_os_totalmem_001.js
+++ njs_os_totalmem_001.js
@@ -1,7 +1,7 @@
 // Dedicated to God the Father
 // All Rights Reserved Christopher Topalian Copyright 2000-2022
 // node njs_os_totalmem_001.js
-// shows the total amount of system memory in bytes using console.log
+// shows the total amount of system memory (ram) in bytes using console.log
 
 const os = require('os');
 
