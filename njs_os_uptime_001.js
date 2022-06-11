cb6810d4bf3b7da10850302b338c20f57eb6f73b
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:25:49 -0400

    Add files via upload

---
+++ njs_os_uptime_001.js
@@ -0,0 +1,7 @@
+// Dedicated to God the Father
+// node njs_os_uptime_001.js
+// shows the uptime of the operating system in seconds using console.log
+
+const os = require('os');
+
+console.log(os.uptime());
