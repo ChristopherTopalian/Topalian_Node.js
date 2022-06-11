e7a1c5e58f9c39abe591db757853c619ce046c69
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:51:22 -0400

    Add files via upload

---
+++ njs_os_userInfo_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_userInfo_001.js
+// shows the user info using console.log
+
+const os = require('os');
+
+console.log(os.userInfo());
