3a2e7a797e5c9cf166b08be6c50d16ca4fe76d37
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:50:05 -0400

    Delete njs_os_dirname_001.js

--- njs_os_dirname_001.js
+++
@@ -1,6 +0,0 @@
-// Dedicated to God the Father
-// All Rights Reserved Christopher Topalian Copyright 2000-2022
-// node njs_os_dirname_001.js
-// shows the directory name using console.log
-
-console.log(__dirname);
