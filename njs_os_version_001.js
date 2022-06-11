77fed6b4e29b34f0a53dd0f8aea1894d1c966b94
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:26:21 -0400

    Update njs_os_version_001.js

--- njs_os_version_001.js
+++ njs_os_version_001.js
@@ -1,7 +1,7 @@
 // Dedicated to God the Father
 // All Rights Reserved Christopher Topalian Copyright 2000-2022
 // node njs_os_version_001.js
-// shows the verision of the operating system using console.log
+// shows the version of the operating system using console.log
 
 const os = require('os');
 
