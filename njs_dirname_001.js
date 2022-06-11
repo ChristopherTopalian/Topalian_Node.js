2fffd09283d0797484b9526dfbb6867f51dbc335
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:57:38 -0400

    Add files via upload

---
+++ njs_dirname_001.js
@@ -0,0 +1,6 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_dirname_001.js
+// shows the full path directory name using console.log
+
+console.log(__dirname);
