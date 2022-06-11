01c196a6df31633f7add47d190e356d54e6b2c33
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 17:35:54 -0400

    Add files via upload

---
+++ njs_os_homedir_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_homedir_001.js
+// shows the home directory using console.log
+
+const os = require('os');
+
+console.log(os.homedir());
