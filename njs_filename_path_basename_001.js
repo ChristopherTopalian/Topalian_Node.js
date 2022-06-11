b4fc99bd9e65128b56ab1b27903f4a272e745e93
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 19:13:02 -0400

    Add files via upload

---
+++ njs_filename_path_basename_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_filename_path_basename_001.js
+// shows the file base name of the path using console.log
+
+const path = require('path');
+
+console.log(path.basename(__filename));
