1e77f5c789bbcc018a3e0af9a6c0e468721aca2d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 18:10:09 -0400

    Delete njs_filename_001.js

--- njs_filename_001.js
+++
@@ -1,6 +0,0 @@
-// Dedicated to God the Father
-// All Rights Reserved Christopher Topalian Copyright 2000-2022
-// node njs_filename_001.js
-// shows the absolute path file name using console.log
-
-console.log(__filename);
