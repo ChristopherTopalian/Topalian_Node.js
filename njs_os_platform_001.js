8c8403f028d6d7c66b1803df3cf21fad7565c04c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-11 19:00:44 -0400

    Add files via upload

---
+++ njs_os_platform_001.js
@@ -0,0 +1,8 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_os_platform_001.js
+// shows the platform name of the operating system using console.log
+
+const os = require('os');
+
+console.log(os.platform());
