9c0b96a538771939c5e0c83d984181c6540df0e7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-13 11:10:07 -0400

    Add files via upload

---
+++ njs_fs_folder_make_001.js
@@ -0,0 +1,21 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_fs_folder_make_001.js
+// makes a new directory
+// another word for directory is folder
+// mkdir means make directory
+
+const fs = require('fs');
+
+let folderName = "OurFolder";
+
+let callback = function(theError) {
+    if (theError) {
+        console.log(theError);
+    }
+    else {
+        console.log("Folder created successfully");
+    }
+};
+
+fs.mkdir(folderName, callback);
