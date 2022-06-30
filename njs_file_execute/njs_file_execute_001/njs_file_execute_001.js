240ba2474bad86c0815eb5a48eaa2213b0f2968a
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-30 13:28:31 -0400

    Update njs_file_execute_001.js

--- njs_file_execute/njs_file_execute_001/njs_file_execute_001.js
+++ njs_file_execute/njs_file_execute_001/njs_file_execute_001.js
@@ -4,6 +4,6 @@
 // opens the file that we specify by name and extension
 // the file will open in the default application of the extension type specified
 
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
-exec('page_001.html');
+exec("page_001.html");
