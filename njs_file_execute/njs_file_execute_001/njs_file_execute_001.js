dd18735346f84b08e5505659b6cda2ebe2a71be7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2022-06-30 13:26:36 -0400

    Add files via upload

---
+++ njs_file_execute/njs_file_execute_001/njs_file_execute_001.js
@@ -0,0 +1,9 @@
+// Dedicated to God the Father
+// All Rights Reserved Christopher Topalian Copyright 2000-2022
+// node njs_file_execute_001.js
+// opens the file that we specify by name and extension
+// the file will open in the default application of the extension type specified
+
+const exec = require('child_process').exec;
+
+exec('page_001.html');
